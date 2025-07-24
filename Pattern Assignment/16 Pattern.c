b3b16f99ec7d29ca7e7cc8099499e6ce99fec81c
LadAkanksha <akankshalad2003@gmail.com>
2025-07-24 23:31:06 +0530

    Assignment

---
+++ Pattern Assignment/16 Pattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int row = 0 , col = 0 , i = 0 , j = 0 ;
+
+    printf("\nEnter No.of Rows : ");
+    scanf("%d",&row);
+    printf("\nEnter No.of Columns : ");
+    scanf("%d",&col);
+
+    for(i = 1 ; i <= row ; i++)
+    {
+        for(j = 1 ; j <= col ; j++)
+        {
+            if(i >= j)
+            {
+                 printf(" * ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
